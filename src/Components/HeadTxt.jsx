5807ed20af28d7134839a8bbe376654ae88b24ff
Aswinsudhakar <aswinsudhakaran98@gmail.com>
2023-09-17 20:25:07 +0530

    updates

--- src/Components/HeadTxt.jsx
+++ src/Components/HeadTxt.jsx
@@ -6,7 +6,7 @@ const HeadTxt = () => {
      
       <h3 className="scnd-txt mt-2">Countries Database</h3>
       <p className="third-txt pt-1">
-       List of all countries in one page using restcountries.com rest api.
+       List of all countries in one page using restcountries rest api.
       
       </p>
     </div>
